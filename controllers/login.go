ac9588a6049c54fb2fa9991c2d1f38c03078a83f
Masbro <s2gotosingapore@gmail.com>
2022-08-25 16:52:44 +0700

    Add JWT function In After Login

--- controllers/login.go
+++ controllers/login.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 	"rest_api/helpers"
 	"rest_api/models"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -21,8 +23,23 @@ func CheckLogin(c echo.Context) error {
 	if !res {
 		return echo.ErrUnauthorized
 	}
+	token := jwt.New(jwt.SigningMethodHS256)
 
-	return c.String(http.StatusOK, "Login Success")
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["level"] = "application"
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
 }
 
 func GenerateHashPassword(c echo.Context) error {
