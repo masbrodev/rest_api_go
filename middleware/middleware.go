b789a5baeb2951202eded27c9529a178e66a30ed
Masbro <s2gotosingapore@gmail.com>
2022-08-25 16:52:58 +0700

    Add Middleware Function

---
+++ middleware/middleware.go
@@ -0,0 +1,9 @@
+package middlewares
+
+import (
+	"github.com/labstack/echo/middleware"
+)
+
+var IsAuthenticated = middleware.JWTWithConfig(middleware.JWTConfig{
+	SigningKey: []byte("secret"),
+})
